708dc062366c4a6311ef2d7d00039ae6e32fbe37
Daffa Yosataris <dapaisme@gmail.com>
2026-05-05 20:54:07 +0700

    Install Laravel Breeze dan buat Seeder Akun Admin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
-   module.exports = {
-     content: [
-       "./resources/**/*.blade.php",
-       "./resources/**/*.js",
-       "./resources/**/*.vue",
-     ],
-     theme: {
-       extend: {
-         colors: {
-           // Menambahkan custom color palette Maroon
-           maroon: {
-             50: '#fdf2f2',
-             100: '#fbe4e4',
-             200: '#f8caca',
-             300: '#f2a1a1',
-             400: '#e76c6c',
-             500: '#d94141',
-             600: '#c52727',
-             700: '#a51d1d',
-             800: '#881b1b',
-             900: '#711d1d', // Warna dasar maroon gelap
-             950: '#3e0b0b',
-           },
-         }
-       },
-     },
-     plugins: [],
-   }
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
